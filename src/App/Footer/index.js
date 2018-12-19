b2a9a94b336a866c9b19fd9d6e3baa12f8c66f37
Mayank Agarwal <1994mayankagarwal@gmail.com>
2018-12-19 19:05:52 +0530

    Finished app

---
+++ src/App/Footer/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import Link from '../../Link';
+
+import './style.css';
+
+const Footer = () => (
+  <div className="Footer">
+    <div>
+      <small>
+        <span className="Footer-text">Built by</span>{' '}
+          Mayank Agarwal
+        <span className="Footer-text"> with &hearts;</span>
+      </small>
+    </div>
+  </div>
+);
+
+export default Footer;
---
+++ src/Repository/RepositoryList/index.js
@@ -0,0 +1,56 @@
+import React, { Fragment } from 'react';
+
+import FetchMore from '../../FetchMore';
+import RepositoryItem from '../RepositoryItem';
+
+
+import '../style.css';
+
+const getUpdateQuery = entry => (
+  previousResult,
+  { fetchMoreResult },
+) => {
+
+  if (!fetchMoreResult) {
+    return previousResult;
+  }
+
+  return {
+    ...previousResult,
+    [entry]: {
+      ...previousResult[entry],
+      repositories: {
+        ...previousResult[entry].repositories,
+        ...fetchMoreResult[entry].repositories,
+        edges: [
+          ...previousResult[entry].repositories.edges,
+          ...fetchMoreResult[entry].repositories.edges,
+        ],
+      },
+    },
+  };
+};
+
+const RepositoryList = ({ repositories, loading, fetchMore, entry }) => (
+  <Fragment>
+    {repositories.edges.map(({ node }) => (
+    <div key={node.id} className="RepositoryItem">
+      <RepositoryItem {...node} />
+    </div>
+    ))};
+    <FetchMore
+      loading={loading}
+      hasNextPage={repositories.pageInfo.hasNextPage}
+      variables={{
+        cursor: repositories.pageInfo.endCursor,
+      }}
+      updateQuery={getUpdateQuery(entry)}
+      fetchMore={fetchMore}
+    >
+      Repositories
+    </FetchMore>
+  </Fragment>
+);
+  
+
+export default RepositoryList;
